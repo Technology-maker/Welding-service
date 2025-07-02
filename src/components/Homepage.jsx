9d43ebec95816dee69813ac81873dec181c6f14a
Technology-maker <ys5401519@gmail.com>
2025-07-03 04:14:35 +0530

    image update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -21,43 +21,73 @@ const Homepage = () => {
             <Navbar />
 
             {/* header content   */}
-            <header className=" text-white text-center py-0.5">
-
-                <div id="gallery" className="relative w-full" data-carousel="slide">
-
+            <header className="text-white text-center py-0.5">
+                <div id="gallery" className="relative w-full">
                     <div className="relative h-64 overflow-hidden rounded-b-sm sm:h-80 md:h-96">
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src={Img1} className="w-full h-full object-cover" alt="gate Img" />
+                        <div className="duration-700 ease-in-out" data-carousel-item>
+                            <img src={Img1} className="w-full h-full object-cover" alt="Gate Img" />
                         </div>
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
+                        <div className="hidden duration-700 ease-in-out" data-carousel-item>
                             <img src={Img2} className="w-full h-full object-cover" alt="Home Img" />
                         </div>
                         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src={Img3} className="w-full h-full object-cover" alt="metal image" />
+                            <img src={Img3} className="w-full h-full object-cover" alt="Metal Image" />
                         </div>
                         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src={Img4} className="w-full h-full object-cover" alt="stand img" />
+                            <img src={Img4} className="w-full h-full object-cover" alt="Stand Img" />
                         </div>
                     </div>
 
-                    <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+                    <button
+                        type="button"
+                        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-prev
+                    >
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg
+                                className="w-4 h-4 text-white rtl:rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 6 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M5 1 1 5l4 4"
+                                />
                             </svg>
                             <span className="sr-only">Previous</span>
                         </span>
                     </button>
-                    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+
+                    <button
+                        type="button"
+                        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                        data-carousel-next
+                    >
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                            <svg
+                                className="w-4 h-4 text-white rtl:rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 6 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m1 9 4-4-4-4"
+                                />
                             </svg>
                             <span className="sr-only">Next</span>
                         </span>
                     </button>
                 </div>
             </header>
+
 
 
 
